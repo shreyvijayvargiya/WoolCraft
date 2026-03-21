597c234e6aa17348c16b8bfaf0b7e0e251d25cf0
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2026-03-21 15:28:42 +0530

    vercel analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Analytics />
+			<Component {...pageProps} />
+		</>
+	);
 }
